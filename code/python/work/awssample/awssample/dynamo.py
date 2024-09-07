54b75382d355a5e8c61be3c58099b1e38e48a426
kero <kero1976@outlook.jp>
2024-09-08 01:51:24 +0900

    dynamodb作成中

--- code/python/work/awssample/awssample/dynamo.py
+++ code/python/work/awssample/awssample/dynamo.py
@@ -1,5 +1,5 @@
 import boto3
-from awssample.dynamodb.create.base.create import create_table
+from awssample.dynamodb.create.base.create import create_table_simple
 
 print("START")
 # DynamoDBのクライアントを作成
@@ -8,4 +8,4 @@ dynamodb = boto3.resource('dynamodb', region_name='ap-northeast-1')
 
 
 # テーブルを作成する関数を実行
-create_table(dynamodb, "hoge2")
+create_table_simple(dynamodb, "hoge4")
--- code/python/work/awssample/awssample/dynamodb/create/base/create.py
+++ code/python/work/awssample/awssample/dynamodb/create/base/create.py
@@ -15,46 +15,55 @@ logger.addHandler(console_handler)
 # ログメッセージの出力
 logger.debug("Debug レベルのログメッセージ")
 
-def create_table(resource, table_name: str):
+def create_table_simple(resource, table_name: str):
+    KeySchema=[
+        {
+            'AttributeName': 'id',  # パーティションキーの属性名
+            'KeyType': 'HASH'  # パーティションキー
+        }
+    ]
+
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'id',
+            'AttributeType': 'S'  # 文字列型 (S: String, N: Number, B: Binary)
+        }
+    ]
+    
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 5,  # 読み取りキャパシティ
+        'WriteCapacityUnits': 5  # 書き込みキャパシティ
+    }
+    create_table(resource, table_name, KeySchema, AttributeDefinitions, ProvisionedThroughput)
+
+def create_table(resource, table_name: str, key_schema: list[dict], attribute_definitions: list[dict], provisioned_throughput: dict):
     logger.debug({
         "startus": "start",
         "params": {
             "resource": resource,
-            "table_name": table_name
+            "table_name": table_name,
+            "key_schema": key_schema,
+            "attribute_definitions": attribute_definitions,
+            "provisioned_throughput": provisioned_throughput
         }
     })
     try:
         # テーブルの作成
         table = resource.create_table(
             TableName=table_name,  # テーブル名
-            KeySchema=[
-                {
-                    'AttributeName': 'id',  # パーティションキーの属性名
-                    'KeyType': 'HASH'  # パーティションキー
-                }
-            ],
-            AttributeDefinitions=[
-                {
-                    'AttributeName': 'id',
-                    'AttributeType': 'S'  # 文字列型 (S: String, N: Number, B: Binary)
-                }
-            ],
-            ProvisionedThroughput={
-                'ReadCapacityUnits': 5,  # 読み取りキャパシティ
-                'WriteCapacityUnits': 5  # 書き込みキャパシティ
-            }
+            KeySchema=key_schema,
+            AttributeDefinitions=attribute_definitions,
+            ProvisionedThroughput=provisioned_throughput
         )
 
         # テーブルが作成されるまで待機
         table.meta.client.get_waiter('table_exists').wait(TableName=table_name)
-        print("テーブルが正常に作成されました")
         logger.info({
             "startus": "success",
             "result": table
         })
     
     except Exception as e:
-        print(f"エラーが発生しました: {e}")
         logger.error({
             "startus": "fail",
             "exception": e
